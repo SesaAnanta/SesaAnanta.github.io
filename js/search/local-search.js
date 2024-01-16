d8650f91dc54be28de22dfd94f6dc946fc60d839
SesaAnanta <1874121517@qq.com>
2024-01-16 11:14:35 +0800

    Site updated: 2024-01-16 11:14:31

--- js/search/local-search.js
+++ js/search/local-search.js
@@ -362,3 +362,14 @@ window.addEventListener('load', () => {
     searchClickFn()
   })
 })
+
+if (document.querySelector('#bber-talk')) {
+  var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical', // 垂直切换选项
+    loop: true,
+    autoplay: {
+    delay: 3000,
+    pauseOnMouseEnter: true
+  },
+  });
+}
